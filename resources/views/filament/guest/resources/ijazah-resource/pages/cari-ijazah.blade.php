9a45615cfe5559ba6f49cb85da1eea539107151c
pindoyono <pindoyonopatan@gmail.com>
2024-06-10 10:33:33 +0800

    perbaikan widget

--- resources/views/filament/guest/resources/ijazah-resource/pages/cari-ijazah.blade.php
+++ resources/views/filament/guest/resources/ijazah-resource/pages/cari-ijazah.blade.php
@@ -18,6 +18,12 @@
                     {{-- Input to select the user's ID --}}
                 </x-slot>
 
+                {{-- <p class="text-center">
+                    Selamat datang di Sipandawa - Sistem Pangkalan Data Siswa, solusi digital terdepan untuk menyimpan
+                    dan mengelola data ijazah siswa SD dan SMP di Kabupaten Malinau. Aplikasi ini dirancang untuk
+                    memudahkan akses dan verifikasi data ijazah, memastikan setiap dokumen penting tersimpan dengan aman
+                    dan dapat diakses kapan saja, di mana saja.
+                </p> --}}
                 <p class="text-center">
                 <div class="flex items-center justify-center">
                     <div class="overflow-x-auto">
