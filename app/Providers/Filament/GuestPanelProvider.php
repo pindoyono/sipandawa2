3b63c1dcb15d79ebf9c456aaf230870f6880b5d8
pindoyono <pindoyonopatan@gmail.com>
2024-06-10 10:39:04 +0800

    tes

--- app/Providers/Filament/GuestPanelProvider.php
+++ app/Providers/Filament/GuestPanelProvider.php
@@ -41,7 +41,7 @@ class GuestPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Guest/Widgets'), for: 'App\\Filament\\Guest\\Widgets')
             ->widgets([
                 // Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
