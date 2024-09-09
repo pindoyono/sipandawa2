bbcd88f07d414a5fce4d3099ef5eade8c9cd7d59
pindoyono <pindoyonopatan@gmail.com>
2024-09-09 16:25:34 +0800

    databse sd smp

--- app/Filament/Guest/Widgets/StatsAdminOverview.php
+++ app/Filament/Guest/Widgets/StatsAdminOverview.php
@@ -2,18 +2,23 @@
 
 namespace App\Filament\Guest\Widgets;
 
+use DB;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class StatsAdminOverview extends BaseWidget
 {
+
     protected function getStats(): array
     {
+        $sd = DB::table('sekolahs')->where('jenjang', 'SD')->get()->count();
+        $smp = DB::table('sekolahs')->where('jenjang', 'SMP')->get()->count();
+        // dd($sd);
         return [
             //
-            Stat::make('SMP', '41 Sekolah'),
-            Stat::make('SD', '109 Sekolah'),
-            Stat::make('V1.0', 'Sipandawa'),
+            Stat::make('SMP', $sd . ' Sekolah'),
+            Stat::make('SD', $smp . ' Sekolah'),
+            Stat::make('v1.0', 'Sipandawa'),
         ];
     }
 }
